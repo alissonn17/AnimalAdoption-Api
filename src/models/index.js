ddb3eb1dbf5efc7c393441fcf53ad63fc55708a2
AnicaGabrielli <113793660+AnicaGabrielli@users.noreply.github.com>
2025-05-20 17:21:08 -0300

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -22,7 +22,7 @@ const db = {
 
 // Carrega os modelos
 db.models.Animal = require('./Animal')(sequelize, Sequelize.DataTypes);
-db.models.Adotante = require('./Adotante')
+db.models.Adotante = require('./Adotante')(sequelize, Sequelize.DataTypes);
 
 // Associações entre modelos (quando houver)
 Object.keys(db.models).forEach((modelName) => {
