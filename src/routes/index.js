86aa8b1ed529f45aba74c99094a4a7ab71787810
BaagrieL <raulgabruel12@gmail.com>
2025-05-20 15:16:11 -0300

    feat: chama as rotas de Abrigos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const animalRoutes = require('./animalRoutes');
 const adotanteRoutes = require('./adotanteRoutes');
+const abrigoRoutes = require('./abrigoRoutes');
 const router = express.Router();
 
 
 // Definir todas as rotas
 router.use('/animais', animalRoutes);
 router.use('/adotantes', adotanteRoutes);
+router.use('/abrigos', abrigoRoutes);
 
 // Rota padrão para verificação da API
 router.get('/', (req, res) => {
