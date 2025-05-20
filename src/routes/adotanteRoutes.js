1a355bd75279dfa74b733994671562963dc97524
AnicaGabrielli <113793660+AnicaGabrielli@users.noreply.github.com>
2025-05-20 17:23:11 -0300

    Update adotanteRoutes.js

--- src/routes/adotanteRoutes.js
+++ src/routes/adotanteRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const adotanteController = require('../controllers/adotanteController');
 
-// Rotas CRUD para Adotantes
-router.post('/adotantes', adotanteController.criarAdotante);
-router.get('/adotantes', adotanteController.listarAdotantes);
-router.get('/adotantes/:id', adotanteController.buscarAdotante);
-router.put('/adotantes/:id', adotanteController.atualizarAdotante);
-router.delete('/adotantes/:id', adotanteController.deletarAdotante);
+router.post('/', adotanteController.criarAdotante);
+router.get('/', adotanteController.listarAdotantes);
+router.get('/:id', adotanteController.buscarAdotante);
+router.put('/:id', adotanteController.atualizarAdotante);
+router.delete('/:id', adotanteController.deletarAdotante);
 
 module.exports = router;
